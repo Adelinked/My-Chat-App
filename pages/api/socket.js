3fec953c7e580c3ebd01e14d6df2576ee0017292
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-07-11 11:32:31 +0100

    come back to node v16

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const Socket = async (req, res) => {
   if (!res.socket.server.io) {
-    console.log("New Socket.io server...");
+    //console.log("New Socket.io server...");
     // adapt Next's net Server to http Server
     const httpServer = res.socket.server;
     const io = new Server(httpServer, {
